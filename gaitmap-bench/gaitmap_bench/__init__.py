cc252a7eba809ec7f5e23c6b147fc812fc6c2dd3
Arne Küderle <arne.kuederle@fau.de>
2023-03-08 10:24:46 +0100

    Added (a for now unmodified version) of safe to gaitmap_bench

--- gaitmap-bench/gaitmap_bench/__init__.py
+++ gaitmap-bench/gaitmap_bench/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.1.0"
 
-__all__ = ["save"]
+__all__ = ["save_run"]
 
-from gaitmap_bench._challenge_utils import save
+from gaitmap_bench._challenge_utils import save_run
 
--- gaitmap-bench/gaitmap_bench/_challenge_utils.py
+++ gaitmap-bench/gaitmap_bench/_challenge_utils.py
@@ -1,19 +1,29 @@
-import os
-from typing import Dict, Any
+from pathlib import Path
+from typing import Dict, Any, Union, Tuple, Optional
 
-import pandas as pd
-from gaitmap_challenges import BaseChallenge
+from gaitmap_challenges import save_run as challenge_save_run
+from gaitmap_challenges.challenge_base import BaseChallenge
 
 
-def save(metadata: Dict[str, Any], challenge: BaseChallenge) -> None:
-    """Save the results of an entry in the correct folder structure."""
-    # TODO: Create folder structure based on metadata
-    # Find the file, the function was called from
-    # Check git history and save commit hash with results
-    # If "drity" git repo, save it as unofficial run
-    # Save version of challenge
-    # Collect info about host PC
-    print(metadata)
-    results = pd.DataFrame(challenge.cv_results_)
-    with pd.option_context("display.max_columns", 120):
-        print(results.filter(like="test_per_sample"))
+def save_run(
+    challenge: BaseChallenge,
+    entry_name: Union[str, Tuple[str, ...]],
+    custom_metadata: Dict[str, Any],
+    path: Union[str, Path],
+    stored_filenames_relative_to: Optional[Union[str, Path]] = None,
+    use_git: bool = True,
+):
+    # TODO: For now this just passes all information through.
+    #  In the future, this will chaeck the config and overwrite certain values e.g. the path to point to the correct
+    #  location.
+    #  Further, it will validate that specific custom metadata is present.
+    #  IDEA: When in dev mode, warn that values will be overwritten. Then silently overwrite them in prod mode.
+    #        Or should we actually raise in prod mode?
+    return challenge_save_run(
+        challenge=challenge,
+        entry_name=entry_name,
+        custom_metadata=custom_metadata,
+        path=path,
+        stored_filenames_relative_to=stored_filenames_relative_to,
+        use_git=use_git,
+    )
