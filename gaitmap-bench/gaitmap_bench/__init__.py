b292b619f1c0966d19e3a58f9f3abd7270d0cbd6
Arne Küderle <arne.kuederle@fau.de>
2023-03-13 09:52:46 +0100

    Formatting

--- gaitmap-bench/gaitmap_bench/__init__.py
+++ gaitmap-bench/gaitmap_bench/__init__.py
@@ -5,4 +5,4 @@ __version__ = "0.1.0"
 __all__ = ["save_run", "set_config", "config", "reset_config", "create_config_template"]
 
 from gaitmap_bench._challenge_utils import save_run
-from gaitmap_bench._config import set_config, config, reset_config, create_config_template
+from gaitmap_bench._config import config, create_config_template, reset_config, set_config
