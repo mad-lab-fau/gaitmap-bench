ea8104f71842bcb0d65b082115edd9389723e37b
Arne Küderle <arne.kuederle@fau.de>
2023-06-01 15:12:00 +0200

    Refactored viz

---
+++ gaitmap-challenges/gaitmap_challenges/visualization/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ["SingleMetricBoxplot", "group_by_data_label", "box_plot_matplotlib", "box_plot_bokeh"]
+
+from gaitmap_challenges.visualization._box_plot import (
+    SingleMetricBoxplot,
+    group_by_data_label,
+    box_plot_matplotlib,
+    box_plot_bokeh,
+)
